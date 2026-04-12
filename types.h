e87fe21cce8c6b2b5f57aa7e1e23386685bc6166
ecuadros <ecuadros@spc.org.pe>
2026-04-11 19:08:53 -0500

    feat: define platform-dependent XT type alias for integer compatibility

--- types.h
+++ types.h
@@ -12,9 +12,11 @@ using TI = int;
 using TD = double;
 using TS = string;
 
-// // C++20 style
-// template <typename T> struct Type {
-//     using value = T;
-// };
+// XT must be 32bit integer in Windows and 64bit in Linux
+#if defined(_WIN32) || defined(_WIN64)
+    using XT = int;
+#else
+    using XT = long;
+#endif
 
 #endif // __TYPES_H__
