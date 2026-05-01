6530c568d10aa5724bac8466e0b310e96be211ad
unknown <luisangelvilca@hotmail.com>
2026-04-30 20:36:25 -0500

    Functions FirstThat, pop_front, pop_back and push_back added to linkedlist

--- ListsDemo.cpp
+++ ListsDemo.cpp
@@ -1,5 +1,15 @@
 #include "linkedlist.h"
 
+template <typename Node>
+void Print(Node &value, ostream& os){
+    os << value << ",";
+}
+
+template <typename Node, typename T>
+bool IsGreaterThan(Node &node, T x){
+    return node.getDataRef() > x;
+}
+
 void LinkedListDemo(){
     // 
     LinkedList<DescendingLinkedListTrait<TI>> list1;
@@ -11,15 +21,44 @@ void LinkedListDemo(){
     cout << "Lista descendente: " << list1 << endl;
 
     LinkedList<AscendingLinkedListTrait<TI>> list2;
+    using LI = LinkedList<AscendingLinkedListTrait<TI>>::Node;
     list2.insert(6, 15);
     list2.insert(2, 25);
     list2.insert(9, 35);
     list2.insert(1, 45);
     list2.insert(7, 55);
     cout << "Lista ascendente : " << list2 << endl;
+
+    cout << "Prueba ForEach: " << endl;
+    list2.ForEach(Print<LI>, cout);
+    
+    cout << "Prueba First That: " << endl;
+    auto it = list2.FirstThat(IsGreaterThan<LI, TI>, 7);
+    if (it != list2.end())
+        cout << "Primer mayor a 7   : " << *it << endl;
+    cout << "Fin recorrido con iteradores" << endl;
+
+    cout << "Prueba PushFront: " << endl;
+    list1.push_front(5, 5);
+    cout << "Lista ascendente : " << list1 << endl;
+
+    cout << "Prueba PopFront: " << endl;
+    list1.pop_front();
+    cout << "Lista ascendente : " << list1 << endl;
+
+    LinkedList<AscendingLinkedListTrait<TI>> list3;
+    cout << "Prueba PushBack: " << endl;
+    list3.push_back(1, 10);
+    list3.push_back(2, 20);
+    list3.push_back(3, 30);
+    cout << "Lista ascendente : " << list3 << endl;
+
+    cout << "Prueba PopBack: " << endl;
+    list3.pop_back();
+    cout << "Lista ascendente : " << list3 << endl;
+
 }
 
 void ListsDemo(){
     LinkedListDemo();
-    
 }
--- linkedlist.h
+++ linkedlist.h
@@ -104,18 +104,72 @@ public:
     }
     
     virtual        ~LinkedList() {}
-    virtual void    push_front(value_type value, Ref ref){}
-    virtual auto    pop_front() -> std::pair<value_type, Ref>{ 
+    virtual void   push_front(value_type value, Ref ref) {
+        Node* pTemp = new Node(value, ref, m_pRoot);  //Se crea el Nodo temporal con los datos ingresados que apunta a m_pRoot
+        
+        scoped_lock<mutex> lock(m_mtx);
+        m_pRoot = pTemp;                              //Se actualiza el nodo raiz
+        if (m_size == 0)
+            m_pTail = pTemp;                          //Si la lista esta vacia la cola tambien se debe actualizar
+        ++m_size;
+        delete pTemp;
+    }
+    virtual auto    pop_front() -> pair<value_type, Ref>{ 
+        
+        scoped_lock<mutex> lock(m_mtx);
         if( m_pRoot ){
             Node* pTemp = m_pRoot;
             m_pRoot = m_pRoot->getNext();
-            return std::make_pair(pTemp->getData(), pTemp->getRef());
+            --m_size;
+            return make_pair(pTemp->getData(), pTemp->getRef());
         }else
-            throw std::out_of_range("pop_front(): empty list");
+            throw out_of_range("pop_front(): empty list");
     }
-    virtual void    push_back(value_type value, Ref ref){}
-    virtual auto    pop_back() -> std::pair<value_type, Ref>{
-        return std::pair<value_type, Ref>();
+    virtual void    push_back(value_type value, Ref ref){
+        Node* pTemp = new Node(value, ref, nullptr);  //Como es el ultimo nodo no apunta a nada
+        
+        scoped_lock<mutex> lock(m_mtx);
+        if (m_size == 0){
+            m_pRoot = pTemp;
+            m_pTail = pTemp; 
+        } else {
+            m_pTail->setNext(pTemp);
+            m_pTail = pTemp;
+        }                  
+        ++m_size;
+    }
+    virtual auto    pop_back() -> pair<value_type, Ref>{
+        
+        scoped_lock<mutex> lock(m_mtx);
+        if( !m_pRoot )
+            throw out_of_range("pop_back(): empty list");
+        
+        //Con un solo elemento
+        if( m_pRoot == m_pTail){
+            auto pDelete = make_pair(m_pTail->getData(), m_pTail->getRef());
+
+            delete m_pTail;
+            m_pRoot = nullptr;
+            m_pTail = nullptr;
+            
+            --m_size;
+            return pDelete;
+        }
+
+        //Lista con varios elementos
+        Node *pTemp = m_pRoot;
+
+        while (pTemp->getNext() != m_pTail)             //Recorrer toda la lista hasta el penultimo elemento
+            pTemp = pTemp->getNext();
+
+        auto pDelete = make_pair(m_pTail->getData(), pTemp->getRef());
+        
+        delete pTemp->getNext();
+        pTemp->setNext(nullptr);
+
+        m_pTail = pTemp;
+        --m_size;
+        return pDelete;
     }
 private:
             void    internal_insert(Node* &pParent, const value_type &value, Ref ref);
@@ -134,6 +188,12 @@ public:
     void ForEach(Func func, Args &&...  args){
         unique_lock<mutex> lock(m_mtx);
         ::ForEach(begin(), end(), func, std::forward<Args>(args)... );
+    }
+
+    //Agregar FirstThat
+    template <typename Func, typename... Args>
+    forward_iterator FirstThat(Func func, Args &&...  args){
+        return ::FirstThat(begin(), end(), func, std::forward<Args>(args)... );
     }
 };
 
